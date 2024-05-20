47a7ccbd89f8b4aa82505e93ff173a8ca80530cd
AJHoll <anthony_j_holl@mail.ru>
2024-05-20 22:46:15 +0300

    add base run.sh and update dump

---
+++ backend/run.sh
@@ -0,0 +1,6 @@
+export AUTH_SECRET_KEY="super-secret-key"
+export AUTH_EXPIRES_IN="8h"
+export DATABASE_URL="postgresql://square:gfhjkbr@localhost:5432/square?schema=square"
+export USER_PASSWORD_SALT_LEN=10
+export TEMPLATE_DIR="./templates"
+node main.js
